794ea380260baaf025cf82602c0cdf5816a32538
Melvin Berrios <melvinberrios48@gmail.com>
2024-02-06 03:06:35 -0600

    Proyecto NotiNews

---
+++ src/main/java/com/notinews/notinews/NotinewsApplication.java
@@ -0,0 +1,13 @@
+package com.notinews.notinews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotinewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotinewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notinews/notinews/NotinewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notinews.notinews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotinewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
